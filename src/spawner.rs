853ed2d0031acca981e5c836ed0ebbb53019f068
Ray Tran <rt@elixirsoftware.co.uk>
2021-07-30 20:49:02 +0100

    Add field of view for player

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,3 +1,4 @@
+
 use crate::prelude::*;
 
 pub fn spawn_player(ecs: &mut World, pos: Point) {
@@ -12,6 +13,7 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             current: 10,
             max: 10,
         },
+        FieldOfView::new(8),
     ));
 }
 
@@ -34,6 +36,7 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
             max: hp,
         },
         Name(name),
+        FieldOfView::new(6),
     ));
 }
 
